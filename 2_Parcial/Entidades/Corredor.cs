ec2acbe5c8a1964d27e931ce4c8b7e73ba28ecc1
lucianavarela <lucianavarela@hotmail.es>
2017-11-19 19:10:08 -0300

    commit

---
+++ 2_Parcial/Entidades/Corredor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public abstract class Corredor
+    {
+        public enum Carril { }
+        static protected Random _avance;
+        protected Carril _carrilElegido;
+        short _velocidadMax;
+
+        public Carril CarrilElegido
+        {
+            get { return this._carrilElegido; }
+        }
+
+        public short VelocidadMaxima
+        {
+            get { return this._velocidadMax; }
+            set
+            {
+                if (value <= 10)
+                {
+                    this._velocidadMax = value;
+                }
+            }
+        }
+
+        static Corredor()
+        {
+            _avance = new Random();
+        }
+
+        protected Corredor(short velocidadMax, Carril carril)
+        {
+            this._carrilElegido = carril;
+            this.VelocidadMaxima = velocidadMax;
+        }
+
+        public abstract void Correr();
+
+        public virtual void Guardar(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
